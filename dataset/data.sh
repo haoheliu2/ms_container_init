e335f6a711000cefd69ba0a87753a1ba3fed51d5
haoheliu <867390095@qq.com>
2021-11-27 14:41:28 +0800

    dataset

--- dataset/data.sh
+++ dataset/data.sh
@@ -1,8 +1,10 @@
-mkdir ~/data
+mkdir -p ~/data/LJSpeech-1.1
 echo "Copying LJSpeech-1.1 dataset"
-cp -r /blob/v-jcong/data/LJSpeech-1.1 ~/data
+cp -r /blob/v-jcong/data/LJSpeech-1.1/raw ~/data/LJSpeech-1.1
 echo "Done"
 mkdir -p ~/data/LJSpeech-1.1-fs2
 echo "Copying LJSpeech-1.1 pitch dataset"
 cp -r /blob/v-jcong/data/LJSpeech-1.1-fs2/f0 ~/data/LJSpeech-1.1-fs2
 echo "Done"
+cp -r filelists ~
+echo "Success!"
